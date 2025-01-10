841c22ca59d4fdb09bd4246475f37bddb3d97bdc
ALFAxxs <bahodirerkinov111@gmail.com>
2025-01-10 21:38:50 +0500

    fully

--- CRUD_based_on_function/urls.py
+++ CRUD_based_on_function/urls.py
@@ -6,5 +6,6 @@ app_name = 'func'
 urlpatterns = [
     path('order_func/', views.OrderAddView, name='order_func'),
     path('show_order/', views.ShowOrderView, name='show_orders'),
-    path('delete/<int:id>', views.DeleteOrderView, name='delete_orders')
+    path('delete/<int:id>', views.DeleteOrderView, name='delete_orders'),
+    path('update/<int:id>', views.UpdateOrderView, name='update_orders')
 ]
--- CRUD_based_on_function/views.py
+++ CRUD_based_on_function/views.py
@@ -3,7 +3,18 @@ from CRUD_based_on_forms.models import Orders
 
 
 def OrderAddView(request):
-
+    if request.method in ['POST', 'FILES']:
+        obj = Orders.objects.create(
+        image=request.FILES.get('image'),
+        video=request.FILES.get('video'),
+        fname=request.POST.get('fname'),
+        lname=request.POST.get('lname'),
+        price=request.POST.get('price'),
+        mail=request.POST.get('mail'),
+        addr=request.POST.get('addr')
+        )
+        obj.save()
+        return redirect('func:show_orders')
     template_name = 'crud_func/Orders.html'
     return render(request, template_name)
 
@@ -22,11 +33,39 @@ def DeleteOrderView(request, id):
     if request.method == 'POST':
         if 'confirm' in request.POST:
             obj.delete()
-            return redirect('show_orders')
+            return redirect('func:show_orders')
         else:
-            return redirect('show_orders')
-    template_name = 'crud_func/confirmation.html'
+            return redirect('func:show_orders')
+    template_name = 'crud_func/confirmation2.html'
     context = {
         'obj': obj
     }
     return render(request, template_name, context)
+
+
+def UpdateOrderView(request, id):
+    # Fetch the instance of the order
+    order = get_object_or_404(Orders, id=id)
+
+    if request.method == 'POST':
+        # Update the instance with new data
+        order.image = request.FILES.get('image') if request.FILES.get('image') else order.image
+        order.video = request.FILES.get('video') if request.FILES.get('video') else order.video
+        order.fname = request.POST.get('fname', order.fname)
+        order.lname = request.POST.get('lname', order.lname)
+        order.price = request.POST.get('price', order.price)
+        order.mail = request.POST.get('mail', order.mail)
+        order.addr = request.POST.get('addr', order.addr)
+
+        # Save the updated instance
+        order.save()
+        return redirect('show_orders')  # Redirect after successful update
+
+    # Prepopulate the data in the template
+    template_name = 'crud_func/update_order.html'
+    context = {'order': order}
+    return render(request, template_name, context)
+# def UpdateOrderView(request, id):
+#     obj = get_object_or_404(Orders, id=id)
+#     template_name = 'crud_func/Orders.html'
+#     return render(request, template_name)
